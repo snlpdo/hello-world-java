7fe50c51fbb291bb36bcaf364305b66482e53533
grosbois <grosbois@localhost>
2017-04-06 22:14:31 +0200

    Revenir à la version d'origine

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,6 @@
 public class Hello {
 	Hello() {
 		System.out.println("Hello World!");
-		System.out.println("How are you ?");
-		System.out.println("How old are you ?");
 	}
 
 	public static void main(String[] args) {
