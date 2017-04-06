3ca92fcfff50538f75db4e438809ef37e34b6fe4
grosbois <grosbois@localhost>
2017-04-06 22:08:57 +0200

    Troisième message

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	Hello() {
 		System.out.println("Hello World!");
 		System.out.println("How are you ?");
+		System.out.println("How old are you ?");
 	}
 
 	public static void main(String[] args) {
